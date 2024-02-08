3a0207497547774ae2c412940d1e4620d35ee04e
N3koSempai <alvarom12098@gmail.com>
2024-02-08 04:19:34 -0500

    [feat] added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,65 @@
-import { Button } from '@material-tailwind/react'
+import { Button, CardBody, Card, Typography, ButtonGroup, CardHeader } from '@material-tailwind/react'
 import './App.css'
 import useFlightInfo from './stores/store'
+import flightimg from './assets/BestFlight.png'
 function App () {
-  const { code, changeCode } = useFlightInfo()
-
-  const handlerChangeCode = () => {
-    changeCode('523Block')
-  }
   return (
     <>
-      <div className='flex w-full h-full'>
-        <h1 className='flex  text-3xl font-bold underline'>
+      <div className='MainContainer flex flex-col items-center justify-center w-full'>
 
-          {code || <p> nothing</p>}
-        </h1>
-        <Button onClick={handlerChangeCode}>change to 523Block</Button>
+        <Card
+          className='flex flex-col text-center   justify-center w-auto lg:h-auto'
+          style={{
+            backgroundColor: '#003a65',
+            width: '30%',
+            borderRadius: '1.5rem'
+
+          }}
+        >
+
+          <CardHeader
+            className='self-center bg-transparent relative lg:h-56 mt-4'
+            style={{
+              border: 'none',
+              boxShadow: 'none',
+              outline: 'none'
+            }}
+          >
+            <img
+              className='h-72 w-full object-cover object-center'
+              src={flightimg}
+            />
+          </CardHeader>
+          <CardBody style={{
+            width: '100%'
+          }}
+          >
+            <h1
+              className='self-center text-white'
+              style={{
+                fontFamily: 'Montserrat',
+                fontStyle: 'semiBold'
+              }}
+            >
+              Welcome to DemoFlights
+
+            </h1>
+            <Typography
+              className='lg:mt-24 WelcomeText self-center text-balance text-white leading-relaxed'
+
+            >
+              Welcome to DemoFlight - Your go-to place for booking and searching
+              flights! We kickstart your exciting journey towards remarkable
+              destinations. Discover our exclusive deals and easily find the
+              perfect flight for you. 😊🛫
+            </Typography>
+            <ButtonGroup className='gap-4 lg:mt-6 items-center justify-center'>
+              <Button className='MainButton'> I have a ticket</Button>
+              <Button className='MainButton'> Search available flights</Button>
+            </ButtonGroup>
+          </CardBody>
+        </Card>
+
       </div>
     </>
   )
