5d9d3a13ac9625eb3a8768ae0e7e89fbd47f0231
N3koSempai <alvarom12098@gmail.com>
2024-02-08 01:49:41 -0500

    [fix] change stores location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Button } from '@material-tailwind/react'
 import './App.css'
-import useFlightInfo from '../stores/store'
+import useFlightInfo from './stores/store'
 function App () {
   const { code, changeCode } = useFlightInfo()
 
