2af4041d06241f46c01a7b3ca6af654f122becd6
N3koSempai <alvarom12098@gmail.com>
2024-02-08 01:46:33 -0500

    [feat] added states with zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Button } from '@material-tailwind/react'
 import './App.css'
-
+import useFlightInfo from '../stores/store'
 function App () {
+  const { code, changeCode } = useFlightInfo()
+
+  const handlerChangeCode = () => {
+    changeCode('523Block')
+  }
   return (
     <>
       <div className='flex w-full h-full'>
         <h1 className='flex  text-3xl font-bold underline'>
-          <Button>hello</Button>
-          Hello world!
+
+          {code || <p> nothing</p>}
         </h1>
+        <Button onClick={handlerChangeCode}>change to 523Block</Button>
       </div>
     </>
   )
---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,8 @@
+import './dashboard.css'
+export default function DashboardPage () {
+  return (
+    <div className='MainContainer'>
+      <p>hello</p>
+    </div>
+  )
+}
